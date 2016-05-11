4c138d63ded243ea084ccca24dea55dcc4b9dc9e
ティン・ルーフ <tinruufu@gmail.com>
2016-05-11 13:04:03 +0100

    handle dupes

--- tweet.py
+++ tweet.py
@@ -8,8 +8,28 @@ auth.set_access_token(token_key, token_secret)
 api = tweepy.API(auth)
 
 
+def pad(status):
+    zwsp = '\u200b'
+    return status[0] + zwsp + status[1:]
+
+
 def tweet():
-    api.update_status(status=koo())
+    status = koo()
+    attempts = 0
+
+    while True:
+        attempts += 1
+
+        try:
+            api.update_status(status=status)
+        except tweepy.TweepError as e:
+            if e.api_code == 187 and attempts < 10:
+                # this is a dupe
+                status = pad(status)
+            else:
+                raise
+        else:
+            break
 
 
 if __name__ == '__main__':
